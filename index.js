5d4ec7dc41ae8565fa6f745803ed5efcd56c0d9d
BruTO <bruto_life@inbox.ru>
2022-01-10 11:51:52 +0300

    Добавил работу с недействительными сертификатами

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
-const request = require('request-promise')
-const requestNative = require('request')
-const errors = require('request-promise/errors')
-const ntlm = require('./lib/ntlm')
-const Agent = require('agentkeepalive')
+const request = require("request-promise");
+const requestNative = require("request");
+const errors = require("request-promise/errors");
+const ntlm = require("./lib/ntlm");
+const Agent = require("agentkeepalive");
 
-const startAuth = async options => {
-  var type1msg = ntlm.createType1Message(options)
+const startAuth = async (options) => {
+  var type1msg = ntlm.createType1Message(options);
   options.headers = {
     ...options.headers,
-    Connection: 'keep-alive',
-    Authorization: type1msg
-  }
-  return request(options)
-}
+    Connection: "keep-alive",
+    Authorization: type1msg,
+  };
+  return request(options);
+};
 
 const requestComplete = async (authHeader, options, params, streamCallback) => {
   if (!authHeader) {
-    throw Error('www-authenticate not found on response of second request')
+    throw Error("www-authenticate not found on response of second request");
   }
-  const type2msg = ntlm.parseType2Message(authHeader)
-  const type3msg = ntlm.createType3Message(type2msg, options)
+  const type2msg = ntlm.parseType2Message(authHeader);
+  const type3msg = ntlm.createType3Message(type2msg, options);
 
   options.headers = {
     ...options.headers,
-    Connection: 'keep-alive',
-    Authorization: type3msg
-  }
+    Connection: "keep-alive",
+    Authorization: type3msg,
+  };
 
-  if (typeof params === 'string') options.body = params
-  else options.json = params
-  return streamCallback ? requestNative(options).on('response', streamCallback) : request(options)
-}
+  if (typeof params === "string") options.body = params;
+  else options.json = params;
+  return streamCallback
+    ? requestNative(options).on("response", streamCallback)
+    : request(options);
+};
 
-const makeRequest = method => async (options, params, streamCallback) => {
+const makeRequest = (method) => async (options, params, streamCallback) => {
   const KeepAliveAgent =
-    options.url.toLowerCase().indexOf('https://') === 0
+    options.url.toLowerCase().indexOf("https://") === 0
       ? Agent.HttpsAgent
-      : Agent
+      : Agent;
 
   options = {
-    workstation: '',
-    ntlm_domain: '',
+    workstation: "",
+    ntlm_domain: "",
     headers: {},
     method,
-    agent: new KeepAliveAgent(),
-    ...options
-  }
+    agent: new KeepAliveAgent({
+      rejectUnauthorized: false,
+    }),
+    ...options,
+  };
 
-  let authHeader = ''
+  let authHeader = "";
   await startAuth(options).catch((e) => {
     if (e instanceof errors.StatusCodeError) {
-      authHeader = e.response.headers['www-authenticate']
+      authHeader = e.response.headers["www-authenticate"];
     } else {
-      throw e
+      throw e;
     }
-  })
+  });
 
-  return requestComplete(authHeader, options, params, streamCallback)
-}
+  return requestComplete(authHeader, options, params, streamCallback);
+};
 
-exports.get = makeRequest('get')
-exports.post = makeRequest('post')
-exports.put = makeRequest('put')
-exports.patch = makeRequest('patch')
-exports.delete = makeRequest('delete')
-exports.options = makeRequest('options')
+exports.get = makeRequest("get");
+exports.post = makeRequest("post");
+exports.put = makeRequest("put");
+exports.patch = makeRequest("patch");
+exports.delete = makeRequest("delete");
+exports.options = makeRequest("options");
